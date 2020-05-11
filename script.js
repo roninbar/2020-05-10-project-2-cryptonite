87c007eaf9cbab3ba6c88114a3e6c1f94d77fc86
ron <rinbar70@gmail.com>
2020-05-11 22:23:45 +0300

    Transform coin symbols to uppercase.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(function () {
                     .map(coin => $(`
                         <div class="card col-sm-6 col-md-4 col-lg-3 col-xl-2">
                             <div class="card-body">
-                                <h5 class="card-title">${coin.symbol}</h5>
+                                <h5 class="card-title text-uppercase">${coin.symbol}</h5>
                                 <div class="custom-control custom-switch" style="position: absolute; top: 22px; right: 25px;">
                                     <input type="checkbox" class="custom-control-input" id="${coin.id}">
                                     <label class="custom-control-label" for="${coin.id}"></label>
